e49aebe2dd7c1653bfdcac62eb6652ce77575500
NinaOmejc <nina.omejc@gmail.com>
2025-03-24 15:15:21 +0100

    add mulitple files

--- src/TestContainerPackage.jl
+++ src/TestContainerPackage.jl
@@ -1,22 +1,8 @@
 module TestContainerPackage
 
-using JSON
+include("modify.jl")
 
+export add_numbers, modify_json
 
-function add_numbers(a, b)
-    return a + b
-end
-
-
-function modify_json(input_filename="input.json", output_filename="output.json")
-    input_data = JSON.parsefile(input_filename)
-    println("Read from JSON:", input_data)
-    output_data = Dict("message" => "Hello from Julia!", "original" => input_data)
-
-    open(output_filename, "w") do io
-        JSON.print(io, output_data, 2)  # Pretty-print JSON with 2 spaces
-    end
-    println("Saved $(output_filename)")
-end
 
 end # module TestContainerPackage
---
+++ src/modify.jl
@@ -0,0 +1,18 @@
+using JSON
+
+
+function modify_json(input_filename="input.json", output_filename="output.json")
+    input_data = JSON.parsefile(input_filename)
+    println("Read from JSON:", input_data)
+    output_data = Dict("message" => "Hello from Julia!", "original" => input_data)
+
+    open(output_filename, "w") do io
+        JSON.print(io, output_data, 2)  # Pretty-print JSON with 2 spaces
+    end
+    println("Saved $(output_filename)")
+end
+
+
+function add_numbers(a, b)
+    return a + b
+end
